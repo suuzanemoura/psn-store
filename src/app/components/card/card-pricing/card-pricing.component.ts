3c2e1468833bc3023eac6b4b2a0c3715ac1323c9
Suzane Moura <suuzanemoura@gmail.com>
2023-09-29 23:15:51 -0300

    fix: responsividade do menu-bar no header e no footer

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.scss']
+  templateUrl: './card-pricing.component.html'
 })
 export class CardPricingComponent {
 
